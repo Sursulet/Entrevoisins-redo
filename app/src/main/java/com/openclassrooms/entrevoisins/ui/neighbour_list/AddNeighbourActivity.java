781a0914a8d3639c3df133dfcf5a206012197afa
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-04 10:39:43 +0200

    Update AddNeighbourActivity.java

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/AddNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/AddNeighbourActivity.java
@@ -1,38 +1,26 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.content.Intent;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
-import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.databinding.ActivityAddNeighbourBinding;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.NeighbourApiService;
 
 public class AddNeighbourActivity extends AppCompatActivity {
 
-    //@BindView(R.id.avatar)
-    ImageView avatar;
-    //@BindView(R.id.nameLyt)
-    TextInputLayout nameInput;
-    //@BindView(R.id.phoneNumberLyt)
-    TextInputLayout phoneInput;
-    //@BindView(R.id.addressLyt)
-    TextInputLayout addressInput;
-    //@BindView(R.id.aboutMeLyt)
-    TextInputLayout aboutMeInput;
-    //@BindView(R.id.create)
-    MaterialButton addButton;
+    private ActivityAddNeighbourBinding mBinding;
 
     private NeighbourApiService mApiService;
     private String mNeighbourImage;
@@ -40,8 +28,8 @@ public class AddNeighbourActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_neighbour);
-        //ButterKnife.bind(this);
+        mBinding = ActivityAddNeighbourBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mApiService = DI.getNeighbourApiService();
         init();
@@ -59,29 +47,28 @@ public class AddNeighbourActivity extends AppCompatActivity {
     private void init() {
         mNeighbourImage = randomImage();
         Glide.with(this).load(mNeighbourImage).placeholder(R.drawable.ic_account)
-                .apply(RequestOptions.circleCropTransform()).into(avatar);
-        nameInput.getEditText().addTextChangedListener(new TextWatcher() {
+                .apply(RequestOptions.circleCropTransform()).into(mBinding.avatar);
+        mBinding.nameLyt.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) { }
             @Override
             public void afterTextChanged(Editable s) {
-                addButton.setEnabled(s.length() > 0);
+                mBinding.create.setEnabled(s.length() > 0);
             }
         });
 
     }
 
-    //@OnClick(R.id.create)
     void addNeighbour() {
         Neighbour neighbour = new Neighbour(
                 System.currentTimeMillis(),
-                nameInput.getEditText().getText().toString(),
+                mBinding.nameLyt.getEditText().getText().toString(),
                 mNeighbourImage,
-                addressInput.getEditText().getText().toString(),
-                phoneInput.getEditText().getText().toString(),
-                aboutMeInput.getEditText().getText().toString()
+                mBinding.addressLyt.getEditText().getText().toString(),
+                mBinding.phoneNumberLyt.getEditText().getText().toString(),
+                mBinding.aboutMeLyt.getEditText().getText().toString()
         );
         mApiService.createNeighbour(neighbour);
         finish();
