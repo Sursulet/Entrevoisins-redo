e423e535322fb63f4d71558b11bfc9b7a8a5ffca
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-04 10:59:15 +0200

    Update NeighbourFragment.java

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourFragment.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourFragment.java
@@ -2,15 +2,16 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.content.Context;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.openclassrooms.entrevoisins.R;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.openclassrooms.entrevoisins.databinding.FragmentNeighbourListBinding;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
 import com.openclassrooms.entrevoisins.model.Neighbour;
@@ -24,17 +25,24 @@ import java.util.List;
 
 public class NeighbourFragment extends Fragment {
 
+    private static final String ARG_NEIGHBOUR = "neighbour";
+    private FragmentNeighbourListBinding mBinding;
     private NeighbourApiService mApiService;
     private List<Neighbour> mNeighbours;
     private RecyclerView mRecyclerView;
+
+    private int mIndex = 0;
 
 
     /**
      * Create and return a new instance
      * @return @{@link NeighbourFragment}
      */
-    public static NeighbourFragment newInstance() {
+    public static NeighbourFragment newInstance(int position) {
         NeighbourFragment fragment = new NeighbourFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_NEIGHBOUR, position);
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -42,16 +50,20 @@ public class NeighbourFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mApiService = DI.getNeighbourApiService();
+        if (getArguments() != null) {
+            mIndex = getArguments().getInt(ARG_NEIGHBOUR);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_neighbour_list, container, false);
+        mBinding = FragmentNeighbourListBinding.inflate(inflater, container, false);
+        View view = mBinding.getRoot();
         Context context = view.getContext();
         mRecyclerView = (RecyclerView) view;
         mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
         return view;
     }
 
@@ -59,7 +71,16 @@ public class NeighbourFragment extends Fragment {
      * Init the List of neighbours
      */
     private void initList() {
-        mNeighbours = mApiService.getNeighbours();
+        mNeighbours = (mIndex == 0) ? mApiService.getNeighbours() : mApiService.getFavorite();
+        /*switch (mIndex) {
+            case 0:
+                mNeighbours = mApiService.getNeighbours();
+                mNeighbours.get(1).setFavorite(true);
+                break;
+            case 1:
+                mNeighbours = mApiService.getFavorite();
+                break;
+        }*/
         mRecyclerView.setAdapter(new MyNeighbourRecyclerViewAdapter(mNeighbours));
     }
 
@@ -79,6 +100,12 @@ public class NeighbourFragment extends Fragment {
     public void onStop() {
         super.onStop();
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
     }
 
     /**
