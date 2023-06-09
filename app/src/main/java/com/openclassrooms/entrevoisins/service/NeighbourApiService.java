4e8ecd00686ae87f23778ad4dca4e79f8210a9a8
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-09 21:09:57 +0200

    Update NeighbourApiService.java

--- app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
@@ -12,25 +12,41 @@ public interface NeighbourApiService {
 
     /**
      * Get all my Neighbours
+     *
      * @return {@link List}
      */
     List<Neighbour> getNeighbours();
 
     /**
      * Deletes a neighbour
+     *
      * @param neighbour
      */
     void deleteNeighbour(Neighbour neighbour);
 
     /**
      * Create a neighbour
+     *
      * @param neighbour
      */
     void createNeighbour(Neighbour neighbour);
 
     /**
      * Get all my favorite Neighbours
+     *
      * @return {@link List}
      */
     List<Neighbour> getFavorite();
+
+    /**
+     * Get my Neighbour
+     *
+     * @return {@link Neighbour}
+     */
+    Neighbour getNeighbour(Long id);
+
+    /**
+     * Change my neighbour status of favorite
+     */
+    void changeStatus(Neighbour neighbour);
 }
