cb39524fe3687b14e7b74406b3f935727f6a005d
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-09 21:08:32 +0200

    Update DetailNeighbourFragment.java

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourFragment.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourFragment.java
@@ -8,8 +8,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.bumptech.glide.Glide;
+import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.databinding.FragmentDetailNeighbourBinding;
 import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
@@ -31,7 +33,7 @@ public class DetailNeighbourFragment extends Fragment {
                     .getId();
         }
         mApiService = DI.getNeighbourApiService();
-        mNeighbour = mApiService.getNeighbours().stream().filter(neighbour -> mArg.equals(neighbour.getId())).findFirst().orElse(null);
+        mNeighbour = mApiService.getNeighbour(mArg);
     }
 
     @Override
@@ -44,13 +46,22 @@ public class DetailNeighbourFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         mBinding.toolbar.setTitle(mNeighbour.getName());
+        mBinding.toolbar.setNavigationOnClickListener(vw -> NavHostFragment.findNavController(DetailNeighbourFragment.this).navigateUp());
+
         Glide.with(this).load(mNeighbour.getAvatarUrl()).into(mBinding.detailAvatar);
         mBinding.detailName.setText(mNeighbour.getName());
         mBinding.detailAddress.setText(mNeighbour.getAddress());
         mBinding.detailPhone.setText(mNeighbour.getPhoneNumber());
-        mBinding.detailSocialNetwork.setText(String.format("www.facebook.fr/%s",mNeighbour.getName().toLowerCase()));
+        mBinding.detailSocialNetwork.setText(String.format("www.facebook.fr/%s", mNeighbour.getName().toLowerCase()));
         mBinding.detailAboutMe.setText(mNeighbour.getAboutMe());
+
+        mBinding.favorite.setImageResource(mNeighbour.isFavorite() ? R.drawable.ic_star_white_24dp : R.drawable.ic_star_border_white_24dp);
+        mBinding.favorite.setOnClickListener(v -> {
+            mApiService.changeStatus(mNeighbour);
+            mBinding.favorite.setImageResource(mNeighbour.isFavorite() ? R.drawable.ic_star_white_24dp : R.drawable.ic_star_border_white_24dp);
+        });
     }
 
     @Override
